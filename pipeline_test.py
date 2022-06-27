15391b4e216cb97b23d51af712e2cf8c05c0ae5e
Sherida <103006836+sjacob01@users.noreply.github.com>
2022-06-27 10:54:09 -0400

    Update pipeline_test.py
    Updating code so that all tensorflow data sets can be used to create a neural network model.
    Needed to use the "eval" function to combine module names with the string '.load_data()'

--- pipeline_test.py
+++ pipeline_test.py
@@ -3,15 +3,13 @@ import tensorflow as tf
 import tensorflow_datasets as tfds
 import numpy as np
 import math
+import pymsgbox
 import os
 import umap
 import umap.plot
 import utils
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
-# from emnist import list_datasets
-# list_datasets() #download data set
-# from emnist import extract_training_samples
 from matplotlib import pyplot
 from numpy import exp
 from tensorflow.keras.datasets import *
@@ -20,14 +18,8 @@ from tensorflow.keras.layers import Dense
 from sklearn.metrics import accuracy_score
 from keract import get_activations, display_activations, display_heatmaps
 
-#images, labels = extract_training_samples('letters')
-
-data_set_names =['Cifar10', 'Mnist',
-'Fashion_mnist', 'Cifar100']
-
-
-data_sets =[cifar10.load_data(), mnist.load_data(),
-fashion_mnist.load_data(), cifar100.load_data()]
+# data_sets =[cifar10.load_data(), mnist.load_data(),
+# fashion_mnist.load_data(), cifar100.load_data()]
 
 
 def select_dataset_to_model(options):
@@ -51,8 +43,7 @@ def select_dataset_to_model(options):
 
 
 res = select_dataset_to_model(tfds.list_builders())
-#print(tfds.list_builders()[res])
-#res = select_dataset_to_model(data_set_names)
+
 
 def select_adversarial_dataset(options, model_choice):
     print("\n")
@@ -65,7 +56,8 @@ def select_adversarial_dataset(options, model_choice):
        
     try:
         if (model_choice+1) == int(i):
-            print('Choose another data set. Current selection used to create model.')
+            
+            pymsgbox.alert('Choose another data set. Current selection used to create model.', 'Title')
             return select_adversarial_dataset(options, model_choice)            
         elif 0 < int(i) <= len(options):
             return int(i) - 1
@@ -76,14 +68,20 @@ def select_adversarial_dataset(options, model_choice):
         pass
     return None
 
-adv_res = select_adversarial_dataset(data_set_names, res)
+adv_res = select_adversarial_dataset(tfds.list_builders(), res)
 
 tf.random.set_seed(10)
 
-#tfds.load("mnist", split="train", try_gcs=True)
-(Model_training_data, Model_training_data_labels), (Model_test_data, Model_test_data_labels) =tfds.load(tfds.list_builders()[res], split="train", try_gcs=True)
-#(Model_training_data, Model_training_data_labels), (Model_test_data, Model_test_data_labels) = data_sets[res] 
-(Adversarial_training_data, Adversarial_training_data_labels), (Adversarial_test_data, Adversarial_test_data_labels) = data_sets[adv_res] 
+(Model_training_data, Model_training_data_labels), (Model_test_data, Model_test_data_labels) = eval(tfds.list_builders()[res]+'.load_data()')
+(Adversarial_training_data, Adversarial_training_data_labels), (Adversarial_test_data, Adversarial_test_data_labels) = eval(tfds.list_builders()[adv_res] +'.load_data()')
+
+
+# #(Model_training_data, Model_training_data_labels), (Model_test_data, Model_test_data_labels) = data_sets[res] 
+# (Adversarial_training_data, Adversarial_training_data_labels), (Adversarial_test_data, Adversarial_test_data_labels) = data_sets[adv_res] 
+
+
+
+
 
 Model_training_data = tf.keras.utils.normalize(Model_training_data,axis=1)
 Model_test_data = tf.keras.utils.normalize(Model_test_data,axis=1)
@@ -119,30 +117,29 @@ number_of_epochs = 10
 fit_model(number_of_epochs)
 
 
-# Select where the model is saved to
-def set_path_to_save_model():
-    print("\n")
-    print("Save model to current working directory?")
-    i = input("Enter yes/no: ")
-    try:
-        if i.lower() == 'yes':
-            # Get the current working directory
-            cwd = os.getcwd()
+# # Select where the model is saved to
+# def set_path_to_save_model():
+    # print("\n")
+    # print("Save model to current working directory?")
+    # i = input("Enter yes/no: ")
+    # try:
+        # if i.lower() == 'yes':
+            # # Get the current working directory
+            # cwd = os.getcwd()
 
-            model.save(cwd+'/Current_model')
-            print('Model saved in the "Current_model" folder')
-            return          
+            # model.save(cwd+'/Current_model')
+            # print('Model saved in the "Current_model" folder')
+            # return          
        
-        else: 
-            path = input('Enter the path to save the model: ')
-            model.save(path)
-            return 
-    except:
-        pass
-    return None
+        # else: 
+            # path = input('Enter the path to save the model: ')
+            # model.save(path)
+            # return 
+    # except:
+        # pass
+    # return None
 
-set_path_to_save_model()
+# set_path_to_save_model()
 
-# Reference: https://programmerah.com/python-error-certificate-verify-failed-certificate-has-expired-40374/
-
+# # Reference: https://programmerah.com/python-error-certificate-verify-failed-certificate-has-expired-40374/
 
