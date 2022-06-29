40c905affd4096129d5e60a7bc4b8a64b3b80080
Sherida <103006836+sjacob01@users.noreply.github.com>
2022-06-29 13:46:58 -0400

    Update create_nn_model.py
    Updated the format of the code so that all lines of code are in functions

--- create_nn_model.py
+++ create_nn_model.py
@@ -15,8 +15,9 @@ from keract import get_activations, display_activations, display_heatmaps
 
 
 ## ----------------------------------------------------------------------------------------------
-# This function allows the user to select the data used to create the neural network model
+
 def select_dataset_to_model(options):
+# This function allows the user to select the data used to create the neural network model
     print("\n")
     print("Please select the data set to model:")
 
@@ -36,41 +37,11 @@ def select_dataset_to_model(options):
     return None
 
 
-dataset_to_model = select_dataset_to_model(tfds.list_builders())
 
 
-## ------------------------------------------------------------------------------------------------
-# This section normalizes the data and trains the model
 
-tf.random.set_seed(10)
-
-(Model_training_data, Model_training_data_labels), (Model_test_data, Model_test_data_labels) = eval(tfds.list_builders()[dataset_to_model]+'.load_data()')
-
-Model_training_data = tf.keras.utils.normalize(Model_training_data,axis=1)
-Model_test_data = tf.keras.utils.normalize(Model_test_data,axis=1)
-
-model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Flatten())
-
-nn_layers = int(input("Enter the number of layers desired: "))
-index = 0
-
-while index < nn_layers:
-    nodes = input("Enter the desired number of nodes for layer_" + str(index+1) +": ")
-   
-    layer_name = "hidden_"+str(index+1)
-    model.add(tf.keras.layers.Dense(str(nodes), activation=tf.nn.relu,name=layer_name))
-    index +=1
-else:
-    layer_name = "hidden_"+str(index+1)
-    model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax,name=layer_name))
-
-
-model.compile(optimizer='sgd',loss='sparse_categorical_crossentropy',metrics='accuracy')
-
-
+def fit_model(number_of_epochs, model,Model_training_data,Model_training_data_labels):
 #Train the model on the desired data set
-def fit_model(number_of_epochs):
    
     model.fit(Model_training_data,Model_training_data_labels,epochs=number_of_epochs)
     score = model.evaluate(Model_training_data,Model_training_data_labels)
@@ -78,13 +49,11 @@ def fit_model(number_of_epochs):
     if score[1] < .98:
         number_of_epochs += 10
         return fit_model(number_of_epochs)
- 
-number_of_epochs = 10 
-fit_model(number_of_epochs)
 
-##----------------------------------------------------------------------------------------------
-# This function enables the user to enter the path where the model is saved to
+
+
 def set_path_to_save_model():
+# This function enables the user to enter the path where the model is saved to
     print("\n")
     print("Save model to current working directory?")
     i = input("Enter yes/no: ")
@@ -105,6 +74,49 @@ def set_path_to_save_model():
         pass
     return None
 
-set_path_to_save_model()
+
+def main():
+    
+    #Select the data set to model
+    dataset_to_model = select_dataset_to_model(tfds.list_builders())
+    
+    # This section normalizes the data and trains the model
+    tf.random.set_seed(10)
+    
+    (Model_training_data, Model_training_data_labels), (Model_test_data, Model_test_data_labels) = eval(tfds.list_builders()[dataset_to_model]+'.load_data()')
+
+    Model_training_data = tf.keras.utils.normalize(Model_training_data,axis=1)
+    Model_test_data = tf.keras.utils.normalize(Model_test_data,axis=1)
+
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Flatten())
+
+    nn_layers = int(input("Enter the number of layers desired: "))
+    index = 0
+
+    while index < nn_layers:
+        nodes = input("Enter the desired number of nodes for layer_" + str(index+1) +": ")
+   
+        layer_name = "hidden_"+str(index+1)
+        model.add(tf.keras.layers.Dense(str(nodes), activation=tf.nn.relu,name=layer_name))
+        index +=1
+    else:
+        layer_name = "hidden_"+str(index+1)
+        model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax,name=layer_name))
+
+    model.compile(optimizer='sgd',loss='sparse_categorical_crossentropy',metrics='accuracy')
+
+    number_of_epochs = 10 
+    fit_model(number_of_epochs, model,Model_training_data,Model_training_data_labels)
+    
+    set_path_to_save_model()
+
+    
+
+if __name__ == "__main__":
+    main()
+
+
+
 
 # Reference: https://programmerah.com/python-error-certificate-verify-failed-certificate-has-expired-40374/
