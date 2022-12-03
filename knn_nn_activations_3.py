6fbabcab55ee64db5e9cb24c054a4427321e23b2
Sherida <103006836+sjacob01@users.noreply.github.com>
2022-12-02 22:17:01 -0500

    Add files via upload
    First step of extraction layer activations

--- knn_nn_activations_3.py
+++ knn_nn_activations_3.py
@@ -74,25 +74,6 @@ def class_indices(index_counter,labels_counter,classes,labels):
      
 
 
-def capture_image_activations(input_data,model,data, nodes,h):
-        for e in range(len(input_data)):
-        
-            image=input_data[e:e+1]
-            activations = get_activations(model,image)           
-            layer_activations = activations[h].T
-            layer_activations = np.reshape(layer_activations,nodes)
-            data.append(layer_activations)
-        return data 
-        
-
-def cat_array_data(arrays_of_class_data, input_data, counter): 
-    arrays_of_class_data = input_data[0]
-    
-    for a in range(1,counter): 
-        arrays_of_class_data = arrays_of_class_data  + input_data[a]
-    return arrays_of_class_data 
-
-
 def set_path_to_save_model(model):
 # This function enables the user to enter the path where the model is saved to
     print("\n")
@@ -114,6 +95,7 @@ def set_path_to_save_model(model):
     except:
         pass
     return None
+
 
 def set_path_to_save_file():
 # This function enables the user to set the path of where to save the activations
@@ -139,50 +121,61 @@ def set_path_to_save_file():
 
 def capture_activations(nn_layers,model,Model_test_data,index_counter, labels_counter,total_number_of_classes):
     
+    class_layer_data = []  
     for a in range(total_number_of_classes):
         # Create the csvs to hold the activations of each layer for each number
         var1 = 'class_'
         var2 = str(a)
         
+        layer_data =[]
         for b in range(1,nn_layers+1):
             var3 = '_layer_'
             var4 = str(b)
             var5 = '.csv'
             var6 = var1+var2+var3+var4+var5
             
-                      
             h = 'hidden_'+ str(b)
-            length_class=len(index_counter[a])
-        
+            length=len(index_counter[a])
+            
             nodes = model.layers[b].output_shape[1]
-      
+          
             column_names=[]
             data = []
-            
-            for c in range(nodes):
+
+           
+            for i in range(nodes):
                 var7 = 'node_'
-                var8 = str(c)
+                var8 = str(i)
                 var9 = var7+var8
                 column_names.append(var9)
-                          
-       
+                              
+           
             df=pd.DataFrame(columns = column_names) 
-            
-            for d in range(length_class):
+            for c in range(length):
                       
-                image=Model_test_data[index_counter[a][d:d+1]]
+                image=Model_test_data[index_counter[a][c:c+1]]
                 activations = get_activations(model,image)
                 layer_activations = activations[h]
                 layer_activations = layer_activations.reshape(nodes)
-                df.loc[d] = layer_activations
+                df.loc[c] = layer_activations
                 
-                data.append(str(index_counter[a][d]))
+                data.append(str(index_counter[a][c]))
           
             df.insert(0,'Image_Index',data,True)  
           
             
             df.to_csv(var6, index=False)
-  
+            #print(df)
+            layer_data.append(df)
+            
+        class_layer_data.append(layer_data)
+    
+    return class_layer_data
+            
+     
+            
+      
+            
   
 
 
@@ -192,30 +185,31 @@ def main():
     dataset_to_model = select_dataset_to_model(tfds.list_builders())
 
 
-    # This section normalizes the data and trains the model
+    #This section normalizes the data and trains the model
     tf.random.set_seed(10)
 
     (Model_training_data, Model_training_data_labels), (Model_test_data, Model_test_data_labels) = eval(tfds.list_builders()[dataset_to_model]+'.load_data()')
 
     Model_training_data = tf.keras.utils.normalize(Model_training_data,axis=1)
     Model_test_data = tf.keras.utils.normalize(Model_test_data,axis=1)
-
+   
     model = tf.keras.models.Sequential()
     model.add(tf.keras.layers.Flatten())
 
     nn_layers = int(input("Enter the number of layers desired: "))
 
-    index = 0
-    while index < nn_layers:
+    for index in range(5):
         nodes = input("Enter the desired number of nodes for layer_" + str(index+1) +": ")
-
         layer_name = "hidden_"+str(index+1)
-        model.add(tf.keras.layers.Dense(str(nodes), activation=tf.nn.relu,name=layer_name))
-        index +=1
-    else:
-        layer_name = "hidden_"+str(index+1)
-    
-    model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax,name=layer_name))
+        model.add(tf.keras.layers.Dense(nodes, activation=tf.nn.relu,name=layer_name))
+        print(layer_name)
+        
+    index +=2
+    layer_name = "hidden_"+str(index)
+    print(layer_name)
+    nodes = input("Enter the desired number of nodes for layer_" + str(index) +": ")
+    model.add(tf.keras.layers.Dense(nodes, activation=tf.nn.softmax,name=layer_name))
+      
 
     model.compile(optimizer='sgd',loss='sparse_categorical_crossentropy',metrics='accuracy')
 
@@ -223,7 +217,7 @@ def main():
     fit_model(number_of_epochs, model,Model_training_data,Model_training_data_labels)
 
     #Save the model
-    #set_path_to_save_model(model)
+    # #set_path_to_save_model(model)
 
 
     Model_classes = np.unique(Model_training_data_labels)
@@ -232,11 +226,29 @@ def main():
     
     index_counter=[]
     labels_counter=[]
-    
-  
+    class_layer_data = []
+      
     [index_counter ,labels_counter] = class_indices(index_counter,labels_counter, Model_classes, Model_test_data_labels)
     
-    capture_activations(nn_layers,model,Model_test_data,index_counter, labels_counter,total_number_of_classes)
+    class_layer_data = capture_activations(nn_layers,model,Model_test_data,index_counter, labels_counter,total_number_of_classes)
+    
+    print(len(class_layer_data))
+    print(class_layer_data[0][5])
+    print(class_layer_data[4][5])
+    print(class_layer_data[9][5])
+ 
+    
+    # print(layer_activations[5])
+    # v= index_counter[0]+index_counter[1]+index_counter[2]+index_counter[3]+index_counter[4]+index_counter[5]+index_counter[6]+index_counter[7]+index_counter[8]+index_counter[9]
+    # v_label=labels_counter[0]+labels_counter[1]+labels_counter[2]+labels_counter[3]+labels_counter[4]+labels_counter[5]+labels_counter[6]+labels_counter[7]+labels_counter[8]+labels_counter[9]
+    # arr=np.array(v_label)
+    
+    # mapper = umap.UMAP().fit(var6.iloc[v])
+    # p=umap.plot.points(mapper, labels=arr,color_key_cmap='Paired', background='black')
+    # var7 = 'Layer_'+str(c)+'_nodes_'+str(nodes)
+    # umap.plot.plt.title(var7)
+    # umap.plot.plt.show()
+    
    
   
 
